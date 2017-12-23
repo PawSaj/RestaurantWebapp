1e9d280ed92341d3c08d4c5baba34671663b8837
MoniRoz <monika.rozwadowska@outlook.com>
2017-12-23 18:58:23 +0100

    Added bootstrap, login form.

--- front/app/layouts/main.jsx
+++ front/app/layouts/main.jsx
@@ -1,22 +1,45 @@
 import React from 'react';
 import {NavLink, Route, Redirect, Switch} from 'react-router-dom';
 import {MAIN_PATHS} from '../consts/paths';
+import {Navbar, Nav, NavItem} from 'react-bootstrap';
+
+/* components */
+import Home from '../components/home';
 
 const MainLayout = () => {
     return (
         <div>
-            <div className="side-nav">
-                <nav className="header">
-                    <NavLink exact to={MAIN_PATHS.HOME} activeClassName="active">Strona głowna</NavLink>
-                    <NavLink exact to={MAIN_PATHS.MENU} activeClassName="active">Menu</NavLink>
-                    <NavLink exact to={MAIN_PATHS.RESERVATIONS} activeClassName="active">Rezerwacje</NavLink>
-                    <NavLink exact to={MAIN_PATHS.PROFILE} activeClassName="active">Profil</NavLink>
-                    <a>Wyloguj</a>
-                </nav>
-            </div>
+            <Navbar inverse collapseOnSelect>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <NavLink exact to={MAIN_PATHS.HOME} activeClassName="active">SajRoz Restaurant</NavLink>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <li role="presentation">
+                            <NavLink exact to={MAIN_PATHS.HOME} activeClassName="active">Strona główna</NavLink>
+                        </li>
+                        <li role="presentation">
+                            <NavLink exact to={MAIN_PATHS.MENU} activeClassName="active">Menu</NavLink>
+                        </li>
+                        <li role="presentation">
+                            <NavLink exact to={MAIN_PATHS.RESERVATIONS} activeClassName="active">Rezerwacje</NavLink>
+                        </li>
+                    </Nav>
+                    <Nav pullRight>
+                        <li role="presentation">
+                            <NavLink exact to={MAIN_PATHS.PROFILE} activeClassName="active">Profil</NavLink>
+                        </li>
+                        <NavItem eventKey={1}>Wyloguj</NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+
             <div className="content">
                 <Switch>
-                    <Route exact path={MAIN_PATHS.HOME}>{}</Route>
+                    <Route exact path={MAIN_PATHS.HOME} component={Home}/>
                     <Route exact path={MAIN_PATHS.MENU}>{}</Route>
                     <Route exact path={MAIN_PATHS.RESERVATIONS}>{}</Route>
                     <Route exact path={MAIN_PATHS.PROFILE}>{}</Route>
--- front/webpack.config.js
+++ front/webpack.config.js
@@ -28,12 +28,13 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: /(\.js|\.jsx)$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 loaders: ['babel']
-            }, {
+            },
+            {
                 test: /\.scss$/,
-                loaders: ["style-loader","css-loader","sass-loader"]
+                loaders: ["style-loader","css-loader","sass-loader"],
             },{
                 test: /\.(png|jpg|jpeg|gif)$/,
                 loaders: ['file-loader']
