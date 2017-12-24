f95caaa1f49ecb99f859a5af2dfe4ddfe69016c6
MoniRoz <monika.rozwadowska@outlook.com>
2017-12-24 08:11:41 +0100

    Added categories in menu styling.

--- front/app/components/menu.jsx
+++ front/app/components/menu.jsx
@@ -1,26 +1,49 @@
 import React from 'react';
-import {Table, Grid} from 'react-bootstrap';
+import {Table, Grid, ModalTitle, ModalHeader} from 'react-bootstrap';
 
 const Menu = () => {
     return (
-        <div>
+        <div className="menu">
             <Grid>
-                <Table responsive>
-                    <thead>
-                    <tr>
-                        <th>Nazwa</th>
-                        <th>Składniki</th>
-                        <th>Cena</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>Pizza Capriciosa</td>
-                        <td>ser, szynka, pieczarki</td>
-                        <td>14,50zł</td>
-                    </tr>
-                    </tbody>
-                </Table>
+                <div className="menu-category">
+                    <ModalTitle className="menu-category-title">Pizza</ModalTitle>
+                    <Table responsive>
+                        <thead>
+                        <tr>
+                            <th>Nazwa</th>
+                            <th>Składniki</th>
+                            <th>Cena</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>Pizza Capriciosa</td>
+                            <td>ser, szynka, pieczarki</td>
+                            <td>14,50zł</td>
+                        </tr>
+                        </tbody>
+                    </Table>
+
+                    <ModalTitle className="menu-category-title">Zupy</ModalTitle>
+                    <Table responsive>
+                        <thead>
+                        <tr>
+                            <th>Nazwa</th>
+                            <th>Składniki</th>
+                            <th>Cena</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>Rosół z kury</td>
+                            <td>kura, makaron, marchewka, cebula, pietruszka, seler, por, liście laurowe, ziele
+                                angielskie
+                            </td>
+                            <td>12,50zł</td>
+                        </tr>
+                        </tbody>
+                    </Table>
+                </div>
             </Grid>
         </div>
     )
